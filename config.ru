c0af356767f8726a3602b2a1217aed466586bf25
Jean de Klerk <jadekler@gmail.com>
2016-07-19 21:54:12 -0600

    Caching strategy

--- config.ru
+++ config.ru
@@ -1,2 +1,11 @@
+require_relative './rss'
 require_relative './server'
+
+MyRss.setup_rss
+
+Thread.new {
+  MyRss.recursively_setup_rss
+}
+
+puts 'Running app'
 run MyApp
--- server.rb
+++ server.rb
@@ -1,30 +1,9 @@
 require 'sinatra'
-require 'httparty'
-require 'nokogiri'
 
 class MyApp < Sinatra::Base
   get '/' do
     require 'rss'
 
-    rss = RSS::Maker.make('atom') do |maker|
-      maker.channel.author = 'Jean de Klerk'
-      maker.channel.updated = Time.now.to_s
-      maker.channel.about = 'This channel is about cheap flights'
-      maker.channel.title = 'Cheap Flights'
-
-      response = HTTParty.get('http://www.thriftytraveler.com/')
-      body = response.body
-      page = Nokogiri::HTML(body)
-
-      page.css('h2.post-title a').each do |a|
-        maker.items.new_item do |item|
-          item.link = a['href']
-          item.title = a.text
-          item.updated = Time.now.to_s
-        end
-      end
-    end
-
-    rss.to_s
+    $rss.to_s
   end
 end
